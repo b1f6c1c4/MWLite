669aa50f0e0a4752974b1966cc863746d05d7857
b1f6c1c4 <b1f6c1c4@gmail.com>
2016-08-12 16:59:03 +0800

    redis (not finished)

--- ClusterFacade/app.js
+++ ClusterFacade/app.js
@@ -1,4 +1,5 @@
 ﻿var express = require("express");
+var redis = require("redis");
 var bodyParser = require("body-parser");
 
 var app = express();
@@ -9,9 +10,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var router = express.Router();
+var client = redis.createClient(process.env.REDIS_HOST || "localhost", 6379);
+
+client.on("connect", function () {
+    console.log("Redis connected");
+});
 
 router.get("/", function (req, res) {
     res.json({ "state": "ok" });
+});
+
+router.post("/push", function (req, res) {
+    var grp = req.query.r;
+    var result = 0;
+    (function next(id) {
+        if (id == grp) {
+            res.json({ "status": result });
+            return;
+        }
+        client.rpush("MWLiteWorkLoad", req.body, function (rst) {
+            // TODO
+            next(id + 1);
+        });
+    })(0);
 });
 
 app.use("/", router);
