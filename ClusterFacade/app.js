621d6c0f7dce0df611fce40661647e85e577572b
b1f6c1c4 <b1f6c1c4@gmail.com>
2016-08-12 09:16:00 +0800

    node project

---
+++ ClusterFacade/app.js
@@ -0,0 +1,21 @@
+﻿var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+// all environments
+app.set("port", 23456);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var router = express.Router();
+
+router.get("/", function (req, res) {
+    res.json({ "state": "ok" });
+});
+
+app.use("/", router);
+
+var server = app.listen(app.get("port"), function () {
+    console.log("Express server listening on port " + app.get("port"));
+});
